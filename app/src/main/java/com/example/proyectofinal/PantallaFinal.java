0c6041fa40c22b3cea78bd2435c10fde2b2dbaef
ViajesdelCalvito <jac.gon.iv@gmail.com>
2023-04-25 10:51:30 +0200

    diseño de pantallas

---
+++ app/src/main/java/com/example/proyectofinal/PantallaFinal.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaFinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_final);
+    }
+}
---
+++ app/src/main/java/com/example/proyectofinal/PrimerNivelEuropa.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrimerNivelEuropa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer_nivel_europa);
+    }
+}
